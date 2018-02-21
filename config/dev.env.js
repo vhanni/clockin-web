4cc11de8fbeb06bb90c710b5c53c08c021241c6b
vhanni <bayanipsayoiii@gmail.com>
2018-02-21 22:11:06 +0800

    remove

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"http://192.168.0.172:8337/"'
-})
