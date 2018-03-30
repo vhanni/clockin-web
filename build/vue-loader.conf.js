c5c87210bf5ef7beac3bb9fbfa178da35a64073f
vhanni <bayanipsayoiii@gmail.com>
2018-03-30 23:36:43 +0800

    Edit variables and delete transformToRequire

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,6 +1,6 @@
-var utils = require('./utils')
-var config = require('../config')
-var isProduction = process.env.NODE_ENV === 'production'
+const utils = require('./utils')
+const config = require('../config')
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   loaders: utils.cssLoaders({
@@ -8,17 +8,5 @@ module.exports = {
       ? config.build.productionSourceMap
       : config.dev.cssSourceMap,
     extract: isProduction
-  }),
-  transformToRequire: {
-    'video': 'src',
-    'source': 'src',
-    'img': 'src',
-    'image': 'xlink:href',
-    'b-img': 'src',
-    'b-img-lazy': ['src', 'blank-src'],
-    'b-card': 'img-src',
-    'b-card-img': 'img-src',
-    'b-carousel-slide': 'img-src',
-    'b-embed': 'src'
-  }
+  })
 }
