a01ac167e143f7687e6b7cf4a22ea227c1bc9fb9
vhanni <bayanipsayoiii@gmail.com>
2018-03-30 22:45:01 +0800

    Edited

--- src/backend/vue-axios/axios.js
+++ src/backend/vue-axios/axios.js
@@ -1,5 +1,4 @@
 /* global localStorage */
-
 import axios from 'axios'
 
 const API_URL = process.env.API_URL
@@ -13,4 +12,6 @@ instance.interceptors.request.use(config => {
   }
   return config
 })
+
+
 export default instance
