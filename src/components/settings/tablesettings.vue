bb0fb534db4a0a010ec27c1365ca7072d3950aa7
vhanni <bayanipsayoiii@gmail.com>
2018-03-30 23:10:34 +0800

    add table setting button

---
+++ src/components/settings/tablesettings.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="my-1">
+    <b-button to="/settings/interface" size="sm" id="linksettings"> <icon name="sliders"></icon></b-button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'tablesettings'
+}
+</script>
