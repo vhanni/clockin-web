315eab3d5f92f41aa6d500b37b17af686683119a
vhanni <bayanipsayoiii@gmail.com>
2018-03-30 22:51:35 +0800

    Import new component modules and delete old modules

--- src/store/store.js
+++ src/store/store.js
@@ -4,23 +4,32 @@ import createPersistedState from 'vuex-persistedstate'
 
 import auth from './modules/auth'
 import history from './modules/history'
-import register from './modules/register'
+import alert from './modules/alert'
+import interfaceSettings from './modules/interface'
+import payslipSettings from './modules/payslip'
+import language from './modules/language'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  // strict: true, // only for debugging because REALLY slow
+  // strict: true, // only for debugging
   modules: {
     auth,
     history,
-    register
+    alert,
+    interfaceSettings,
+    payslipSettings,
+    language,
+    
   },
   // Save some states in localStorage as settings
   plugins: [createPersistedState({
     key: 'settings',
     paths: [
       'auth.user',
-      'history.history'
+      'history.history',
+      'interfaceSettings.theme',
+      'language.currentLanguage'
     ]
   })]
 })
