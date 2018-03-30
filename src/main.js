b1d25292d31794fdd9094f06e1e346ca791dccf6
vhanni <bayanipsayoiii@gmail.com>
2018-03-30 23:19:18 +0800

    Import new modules

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'es6-promise/auto'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { store } from './store/store'
 import axios from './backend/vue-axios'
+import VueI18n from 'vue-i18n'
+import messages from './../static/locales/en'
 import BootstrapVue from 'bootstrap-vue'
+import VueMomentLib from 'vue-moment-lib'
 import VueProgressBar from 'vue-progressbar'
-import './mixins/toasts'
 
-Vue.use(BootstrapVue)
 
 // config
 Vue.config.productionTip = false
 Vue.config.site_url = process.env.SITE_URL
 
+
+Vue.use(BootstrapVue)
+Vue.use(VueMomentLib)
 // icon fontawesome
+import 'vue-awesome/icons/github'
+import 'vue-awesome/icons/sliders'
 import 'vue-awesome/icons/spinner'
 import Icon from 'vue-awesome/components/Icon'
 Vue.component('icon', Icon)
@@ -36,13 +44,77 @@ const options = {
   inverse: false
 }
 
-// if you run dev disable protocol
-if (location.protocol != 'https:') {
-  location.href = `https:${window.location.href.substring(window.location.protocol.length)}`
-}
-
 Vue.use(VueProgressBar, options)
 
+
+Vue.axios.interceptors.response.use(function (response) {
+  if (response.config.url.substr(-4) === '.css' || typeof response.data.gen !== 'undefined' || 
+    (typeof response.data.success !== 'undefined' && response.data.success === true)) {
+    // If this isn't first load of app, finish progress bar
+    if (!window.firstLoad) {
+      Vue.prototype.$Progress.finish()
+    }
+    window.firstLoad = false
+    return response
+  } else {
+    // Despite 200 header code, this seems like a fail
+    Vue.prototype.$Progress.fail()
+    return Promise.reject(response)
+  }
+}, function (err) {
+  // Only after initial load
+  Vue.prototype.$Progress.fail()
+  // If invalid token, remove token from store and alert user
+  if (typeof err.response !== 'undefined' && 
+      typeof err.response.data !== 'undefined' &&
+      typeof err.response.data.error !== 'undefined' &&
+      (err.response.data.error === 'token_invalid' ||
+       err.response.data.error === 'TOKEN_EXPIRED'
+      )) {
+    // Log user out
+    store.dispatch('auth/guest', true)
+    store.dispatch('clearhistory', true)
+  } setTimeout(function(){ 
+    window.location.replace('/')
+  }, 3500)
+})
+// Languages
+Vue.use(VueI18n)
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  fallbackLocale: 'en',
+  messages: {
+    'en': messages
+  } // set locale messages
+})
+
+router.beforeEach((to, from, next) => {
+  var timeout = 0
+  function nextPath () {
+    // If we don't need auth, just go already
+    if (!to.meta.auth) {
+      next()
+    }
+    // Check if we already loaded user state
+    if (store.getters['auth/Isloading']) {
+      timeout++
+      // Retry for 15 seconds
+      if (timeout < 300) setTimeout(nextPath, 50)
+      return
+    }
+    // Check if we need to be logged in for this component
+    if (to.meta.auth && !store.getters['auth/currentUser']) {
+      // if route requires auth and user isn't authenticated
+      next({ path: '/' })
+    } else {
+      next()
+    }
+  }
+  nextPath()
+})
+
+// Little function for quick router inline event function
 window.routerLink = e => {
   e = e || window.event
   e.preventDefault()
@@ -56,5 +128,6 @@ window.vm = new Vue({
   axios,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  i18n
 })
