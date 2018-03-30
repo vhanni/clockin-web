18c8159eb1fa24360be06b00bf2cf42c75dbb244
vhanni <bayanipsayoiii@gmail.com>
2018-03-30 23:26:10 +0800

    added site url for development

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  SITE_URL: '"http://timezonedev.com/"',
   API_URL: '"https://api.clockin.space/"'
 })
