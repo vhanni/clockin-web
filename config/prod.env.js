cd3ea4516f1ee292bd62146c6fdfe8362e2f794d
vhanni <bayanipsayoiii@gmail.com>
2018-05-24 17:33:45 +0800

    edited

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,15 +1,16 @@
 const islocal = process.env.LOCAL === 'true';
 let SITE_URL, TITLE;
 if (islocal) {
-  SITE_URL = '"http://timezone.local/"';
-  TITLE = 'Timezone-Local';
+	SITE_URL = '"http://timezone.com/"';
+	TITLE = 'Timezone-Local';
 } else {
-  SITE_URL = '"https://clockin.space/"';
-  TITLE = 'ClockIn';
+	SITE_URL = '"https://clockin.space/"';
+	TITLE = 'ClockIn';
 }
-module.exports = {
-  NODE_ENV: '"production"',
-  SITE_URL,
-  TITLE,
-  API_URL: '"https://api.clockin.space/"'
+
+export default {
+	NODE_ENV: '"production"',
+	SITE_URL,
+	TITLE,
+	API_URL: '"https://api.clockin.space/"'
 };
